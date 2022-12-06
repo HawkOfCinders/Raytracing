899d10dfb498dbe68b1bfd5cdcb2253a5a2812ed
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2022-12-06 23:11:11 +0200

    Changed raytracing pipeline to align with the GPU pipelines more

--- Raytracing/src/Renderer.cpp
+++ Raytracing/src/Renderer.cpp
@@ -19,15 +19,13 @@ namespace Utils
 
 void Renderer::Render(const Scene& scene, const Camera& camera)
 {
-	Ray ray;
-	ray.Origin = camera.GetPosition();
+	m_ActiveScene = &scene;
+	m_ActiveCamera = &camera;
 
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) {
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
-			ray.Direction = camera.GetRayDirections()[x + y * (float)m_FinalImage->GetWidth()];
-
-			glm::vec4 color = TraceRay(scene, ray);
+			glm::vec4 color = PerPixel(x, y);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
@@ -37,22 +35,35 @@ void Renderer::Render(const Scene& scene, const Camera& camera)
 	m_FinalImage->SetData(m_ImageData);
 }
 
-
-glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray)
+glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 {
-	glm::vec4 clearColor = glm::vec4(scene.ClearColor, 1.0f);
-	glm::vec3 lightdir = glm::vec3(-1, -1, -1);
-	lightdir = glm::normalize(lightdir);
+	Ray ray;
+	ray.Origin = m_ActiveCamera->GetPosition();
+	ray.Direction = m_ActiveCamera->GetRayDirections()[x + y * (float)m_FinalImage->GetWidth()];
+	Renderer::HitPayload payload = TraceRay(ray);
 
-	if (scene.Spheres.size() <= 0)
+	if (payload.HitDistance < 0) 
 	{
-		return clearColor;
+		return glm::vec4(m_ActiveScene->ClearColor, 1.0f);
 	}
 
-	const Sphere* closestSphere = nullptr;
+	glm::vec3 lightdir = glm::normalize(glm::vec3(-1, -1, -1));
+	float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightdir), 0.0f);
+	const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+
+	glm::vec4 pixelColor = glm::vec4(sphere.Albedo * lightIntensity, 1.0f);
+	return pixelColor;
+}
+
+
+
+Renderer::HitPayload Renderer::TraceRay(const Ray& ray)
+{
+	int closestSphere = -1;
 	float hitDistance = std::numeric_limits<float>::max();
 
-	for (const Sphere& sphere : scene.Spheres) {
+	for (size_t i = 0; i < m_ActiveScene->Spheres.size(); i++) {
+		const Sphere& sphere = m_ActiveScene->Spheres[i];
 		glm::vec3 origin = ray.Origin - sphere.Position;
 
 		float a = glm::dot(ray.Direction, ray.Direction);
@@ -74,23 +85,41 @@ glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray)
 		if (closestT < hitDistance) 
 		{
 			hitDistance = closestT;
-			closestSphere = &sphere;
+			closestSphere = (int)i;
 		}
 	}
 
-	if (closestSphere == nullptr)
+	if (closestSphere < 0)
 	{
-		return clearColor;
+		return Miss(ray);
 	}
 
-	glm::vec3 origin = ray.Origin - closestSphere->Position;
+	return ClosestHit(ray, hitDistance, closestSphere);
+}
 
-	glm::vec3 hitPosition = origin + ray.Direction * hitDistance;
-	glm::vec3 normal = glm::normalize(hitPosition);
+Renderer::HitPayload Renderer::ClosestHit(const Ray& ray, float hitDistance, int objectIndex)
+{
+	HitPayload payload;
+	payload.HitDistance = hitDistance;
+	payload.ObjectIndex = objectIndex;
 
-	float lightIntensity = glm::max(glm::dot(normal, -lightdir), 0.0f);
-	return glm::vec4(closestSphere->Albedo * lightIntensity, 1.0f);
+	const Sphere& closestSphere = m_ActiveScene->Spheres[objectIndex];
 
+	glm::vec3 origin = ray.Origin - closestSphere.Position;
+
+	payload.WorldPosition = origin + ray.Direction * hitDistance;
+	payload.WorldNormal = glm::normalize(payload.WorldPosition);
+	
+	payload.WorldPosition += closestSphere.Position;
+
+	return payload;
+}
+
+Renderer::HitPayload Renderer::Miss(const Ray& ray)
+{
+	HitPayload payload;
+	payload.HitDistance = -1.0f;
+	return payload;
 }
 
 void Renderer::CreateOrResizeImage(uint32_t width, uint32_t height)
--- Raytracing/src/Renderer.h
+++ Raytracing/src/Renderer.h
@@ -10,14 +10,32 @@ class Renderer
 {
 public:
 	Renderer() = default;
-
 	void Render(const Scene& scene, const Camera& camera);
 	void CreateOrResizeImage(uint32_t width, uint32_t height);
-
-
 	std::shared_ptr<Walnut::Image> GetFinalImage();
+
 private:
-	glm::vec4 TraceRay(const Scene& scene, const Ray& ray);
+
+	struct HitPayload
+	{
+		float HitDistance;
+		glm::vec3 WorldPosition;
+		glm::vec3 WorldNormal;
+
+		int ObjectIndex;
+	};
+
+	//Methods
+	glm::vec4 PerPixel(uint32_t x, uint32_t y); //RayGen in the GPU API naming
+	HitPayload TraceRay(const Ray& ray);
+	HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex);
+	HitPayload Miss(const Ray& ray);
+
+	// Member variables
 	std::shared_ptr<Walnut::Image> m_FinalImage;
+	const Scene* m_ActiveScene = nullptr;
+	const Camera* m_ActiveCamera = nullptr;
 	uint32_t* m_ImageData = nullptr;
+
+
 };
